35f505dd81957028a86d45b65556fef0d54247ce
clintmann <clintmann3@gmail.com>
2018-03-28 22:44:35 -0400

    updating run.sh

---
+++ fingerprint-app/run.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+echo
+echo "################################################"
+echo "Thank you for using the Network Fingerprint App "
+echo "--"
+echo "This script will set up the environment to"
+echo "monitor the Spark Room."
+echo "--"
+echo "It will also setup the application that makes"
+echo "API calls to your Cisco Identity Services Engine"
+echo
+echo "################################################"
+echo
+
+
+echo "Press Enter to continue..."
+read confirm
+
+echo "Please enter YOUR Spark AUTHENTICATION TOKEN : "
+read -s TOKEN
+
+echo "Please enter the Spark AUTHENTICATION TOKEN of the BOT : "
+read -s BOTTOKEN
+
+echo "Please enter the Spark ROOM ID : "
+read -s ROOMID
+
+echo "Next we will set up AUTHENICATION for ISE"
+echo "-----------------------------------------"
+echo "Press Enter to continue..."
+read confirm
+
+echo "Please enter the IP ADDRESS of the ISE SERVER : "
+read -s ISEIPADDR
+
+echo "Please enter ISE USERNAME (must have MnT Admin rights) : "
+read  ISEUSER
+
+echo "Please enter the PASSWORD for the USER entered above : "
+read -s ISEUSERPWD
+
+
+docker run -de TOKEN=$TOKEN \
+    -e APIC_BOTTOKEN=$BOTTOKEN \
+    -e APIC_PASSWORD=$ROOMID \
+    -e ISEIPADDR=$ISEIPADDR \
+    -e ISEUSER=$ISEUSER \
+    -e ISEUSERPWD=$ISEUSERPWD \
+    climann2/network_fingerprint:i386
